5ab9eea318d97fc6c8b9ef2cc09041b9c1ab70b3
Seweryn Golba <sgoolb@gmail.com>
2023-05-16 14:08:45 +0200

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
+const money = document.querySelector("#currencies");
+const input = document.querySelector("#money");
+const result = document.querySelector(".counted");
 const btn = document.getElementById("btn");
+
+let currencyValue;
+const apiUrl = {
+  EUR: "https://api.nbp.pl/api/exchangerates/rates/a/eur/",
+  USD: "https://api.nbp.pl/api/exchangerates/rates/a/usd/",
+  CHF: "https://api.nbp.pl/api/exchangerates/rates/a/chf/",
+};
+
+const selectedCurrency = money.value;
+const url = apiUrl[selectedCurrency];
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const currencyList = data.rates;
+    currencyValue = currencyList[0].mid;
+  })
+  .catch((error) => alert(error));
+
+money.addEventListener("change", () => {
+  const selectedCurrency = money.value;
+  const url = apiUrl[selectedCurrency];
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const currencyList = data.rates;
+      currencyValue = currencyList[0].mid;
+    })
+    .catch((error) => alert(error));
+});
+
+const calculate = () => {
+  const amount = input.value;
+  const calculatedValue = amount * currencyValue;
+  result.innerHTML = calculatedValue.toFixed(2) + " PLN";
+};
 
 btn.addEventListener("click", () => {
   btn.classList.add("spin-animation");
+  setTimeout(() => {
+    calculate();
+    btn.classList.remove("spin-animation");
+  }, 2000);
 });
